86344c8d3cd7361be4b5d40280efba6631864bd3
lbtremblay <48693611+lbtremblay@users.noreply.github.com>
2022-11-02 10:31:45 -0400

    Update src/par_get.f90
    Update comment statement

--- src/par_get.f90
+++ src/par_get.f90
@@ -71,7 +71,7 @@
       phi       =  45d0              ! internal angle of friction
       Cohe      =  10d3              ! cohesion (tensile strght) [N/m2]
       sigc      =  -Cohe*5d8         ! compressive strength cut-off [N/m2]
-      sigt      =  Cohe*5d8          ! compressive strength cut-off [N/m2]
+      sigt      =  Cohe*5d8          ! tensile strength cut-off [N/m2]
       Young     =  1d9               ! Young's Modulus of sea ice
       Poisson   =  3.3d-01           ! Poisson Ratio of sea ice
       lambda0   =  1d5               ! viscous relaxation timescale for sea ice
